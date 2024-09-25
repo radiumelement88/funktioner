2c6d64a233208d090341d993aaa7f75e57362d33
radiumelement88 <elgu07003@utb.vaxjo.se>
2024-09-25 13:58:56 +0200

    Update trianglar.py

--- trianglar.py
+++ trianglar.py
@@ -1,7 +1,7 @@
 tal = int(input("Enter a positiv odd number: "))
 
 if tal%2 == 0 or tal < 0:
-    print("Dissallowed number entered.")
+    print("Disallowed number entered.")
 
 else:
     while tal >= 1:
